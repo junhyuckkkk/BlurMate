f113b13d1816ed8f24042a300a883e68a37a4a6e
junhyuk Kwon <junhyuckkkk@naver.com>
2026-02-09 17:42:51 +0900

    Initial Commit

---
+++ BlurMate/BlurMateApp.swift
@@ -0,0 +1,32 @@
+//
+//  BlurMateApp.swift
+//  BlurMate
+//
+//  Created by 권준혁 on 2/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BlurMateApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
